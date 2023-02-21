55504a7e78062760d95889da5a41e841e1f37685
vonas <24623262+vonas@users.noreply.github.com>
2023-02-21 16:24:02 +0100

    Add helper method for debugging

--- src/main/java/net/snrk/eventchests/EventChestsMod.java
+++ src/main/java/net/snrk/eventchests/EventChestsMod.java
@@ -6,9 +6,13 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.text.Text;
+import net.minecraft.util.annotation.Debug;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_NORMAL;
 
 public class EventChestsMod implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("event-chests");
@@ -49,5 +53,14 @@ public class EventChestsMod implements ModInitializer {
         KeyBinding result = new KeyBinding("key.eventchests." + key, code, KEY_CATEGORY);
         KeyBindingHelper.registerKeyBinding(result);
         return result;
+    }
+
+    /* Useful during debug to make sure the mouse is visible/not captured when a breakpoint is hit.
+     */
+    @Debug
+    @SuppressWarnings("unused")
+    public static boolean _debuggerReleaseControl() {
+        GLFW.glfwSetInputMode(MinecraftClient.getInstance().getWindow().getHandle(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        return true;
     }
 }
