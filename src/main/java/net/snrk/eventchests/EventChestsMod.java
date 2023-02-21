8202fdf9ff4aa2b10286589423f92bd558f93c94
vonas <24623262+vonas@users.noreply.github.com>
2023-02-21 17:34:28 +0100

    Downgrade to 1.19.2 Currently the more supported version of minecraft

--- src/main/java/net/snrk/eventchests/EventChestsMod.java
+++ src/main/java/net/snrk/eventchests/EventChestsMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.text.Text;
 import net.minecraft.util.annotation.Debug;
@@ -53,12 +53,12 @@ public class EventChestsMod implements ModInitializer {
     public static void sendCommand(String command) {
         String modifiedCommand = command.startsWith("/") ? command.substring(1) : command;
         MinecraftClient client = MinecraftClient.getInstance();
-        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
-        if (networkHandler == null) {
+        ClientPlayerEntity player = client.player;
+        if (player == null) {
             printFeedback("Befehl konnte nicht gesendet werden.");
             return;
         }
-        client.getNetworkHandler().sendCommand(modifiedCommand);
+        client.player.sendCommand(modifiedCommand);
     }
 
     private KeyBinding registerKey(String key, int code) {
